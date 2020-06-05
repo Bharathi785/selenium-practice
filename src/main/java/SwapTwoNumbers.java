fe6254bf6dc55f8926ab24a7a442b9901c4abd63
Bharathi785 <bharathibalaji.b@gmail.com>
2020-06-05 12:13:24 +0530

    created programs

---
+++ src/main/java/SwapTwoNumbers.java
@@ -0,0 +1,19 @@
+public class SwapTwoNumbersTempVariable {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 30;
+        System.out.println("before swapping");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("after swapping");
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("after swapping");
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
